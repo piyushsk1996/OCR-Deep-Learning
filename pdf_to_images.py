5126a27fb7dded4c23d42224b14113ec221fe34b
piyushsk1996 <piyush.kshirsagar@triarqhealth.com>
2020-08-27 13:31:19 +0530

    Updated Code added requirements.txt

---
+++ pdf_to_images.py
@@ -0,0 +1,7 @@
+from pdf2image import convert_from_path
+
+pages = convert_from_path("PDF's/test.pdf", 500)
+
+for count, page in enumerate(pages):
+    print(count)
+    page.save('Images_from_pdf/' + str(count) + '.jpg', 'JPEG')
